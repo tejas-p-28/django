4b91ecb812befb8611274b01ee33695b881239ca
Tejas Patil <tejas.patiltp2802@gmail.com>
2024-11-22 17:28:42 +0530

    django learning

--- hello/home/views.py
+++ hello/home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -16,4 +19,11 @@ def services(request):
     return render(request,'services.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, desc = desc, date=datetime.today())
+        contact.save()
+        messages.SUCCESS(request, 'Your form has been submitted')
     return render(request,'contact.html')
